d1e96229486eb3013f9d3488789f20ee8f983250
Person890 <natalie.stepankevycova@sjsu.edu>
2023-03-03 17:33:02 -0800

    mod

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} alt="logo" />
         <p>
-          This is a new react app
+          This is a new react app.
         </p>
         <a
           className="App-link"
